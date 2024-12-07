403a256eaaa2efba6b4475fa17662a022ce5fe6f
Purnank25 <purnankexcel1@gmail.com>
2024-12-07 11:24:55 +0530

    Update main.c all option done by me

--- main.c
+++ main.c
@@ -3,6 +3,16 @@ int main(int argc, char *argv[]) {
 	lineList l;
 	init(&l);
 	readFile(&l, argv[2]);
-	search(&l, argv[1]);
+	search(&l, argv[1]); // normal search 
+	//int count = opt_c(&l , argv[1]); // -c option
+	//opt_b(&l , argv[1],argv[2]); 
+	//printf("%d\n", count);
+	
+	//opt_i(&l, argv[1]); 
+	//opt_v(&l, argv[1]); 
+	//opt_w(&l,argv[1]);
+	//opt_h(&l,argv[1]);
+	//opt_m(&l,argv[1], 70);
+	  search_in_directory(argv[2], argv[1]);
 	return 0;
 }
